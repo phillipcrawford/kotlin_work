2aeb52c150e5e6e7410511ad53ec2ad20c5e1650
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-20 14:29:03 -0700

    more

--- bounty-board/src/main/kotlin/main.kt
+++ bounty-board/src/main/kotlin/main.kt
@@ -1,6 +1,12 @@
+const val heroName = "Madrigal"
+
 fun main() {
-    println("Hello world")
-    val playerLevel: Int = 4
+    println("Hero announces her presence to the world.")
+    println(heroName)
+    var playerLevel: Int = 4
+    println(playerLevel)
+    println("The hero gets a sword")
+    playerLevel++
     println(playerLevel)
 
 }
