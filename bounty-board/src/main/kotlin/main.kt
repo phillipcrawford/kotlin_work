597a4fe13c1a10cc2b9431ef3b71a37d5b5a97df
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-21 14:06:32 -0700

    when expression

--- bounty-board/src/main/kotlin/main.kt
+++ bounty-board/src/main/kotlin/main.kt
@@ -19,4 +19,24 @@ fun main() {
     }
 
     println(faction)
+
+    val hasBefriendedBarbarians = true
+    val hasAngeredBarbarians = false
+    val playerClass = "paladin"
+    val quest: String = when (playerLevel) {
+        1 -> "Meet Mr. Bubbles in the land of soft things."
+        in 2..5 -> {
+            val canTalkToBarbarians = !hasAngeredBarbarians &&
+                    (hasBefriendedBarbarians || playerClass == "barbarian")
+            if (canTalkToBarbarians) {
+                "Convince the barbarians to call off their invasion."
+            } else {
+                "Save the town from the barbarian invasions."
+            }
+        }
+        6 -> "Locate the enchanted sword."
+        7 -> "Recover the long-lost artifact of creation."
+        8 -> "Defeat Nogartse, bring of death and eater of worlds."
+        else -> "There are no quests right now."
+    }
 }
