d68909c4d87f0cf7c22d80eff503e93a9f6d9465
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-21 12:48:15 -0700

    update for solstice

--- bounty-board/src/main/kotlin/main.kt
+++ bounty-board/src/main/kotlin/main.kt
@@ -4,7 +4,7 @@ fun main() {
     println("Hero announces her presence to the world.")
     println(HERO_NAME)
     var playerLevel: Int = 4
-    
+
     println(playerLevel)
     println("The hero gets a sword")
     playerLevel++
