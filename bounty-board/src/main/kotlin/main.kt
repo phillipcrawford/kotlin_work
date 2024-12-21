a116761912fc204b4701a8950db04bcbc097e4ec
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-21 12:52:34 -0700

    when conditionals

--- bounty-board/src/main/kotlin/main.kt
+++ bounty-board/src/main/kotlin/main.kt
@@ -10,4 +10,13 @@ fun main() {
     playerLevel++
     println(playerLevel)
 
+    val race = "gnome"
+    val faction = when (race) {
+        "dwarf" -> "Keepers of the Mines"
+        "gnome" -> "Tinkerers of the Underground"
+        "orc", "human" -> "Free people of the Rolling Hills"
+        else -> "Shadow Cabal of the Unseen Realm" // Unknown race
+    }
+
+    println(faction)
 }
