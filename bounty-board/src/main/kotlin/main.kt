df6b5a567f442fe8bf5becc7ad1a7790a2b6949d
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-25 18:01:27 -0700

    assign om symbol

--- bounty-board/src/main/kotlin/main.kt
+++ bounty-board/src/main/kotlin/main.kt
@@ -34,6 +34,11 @@ fun main() {
     playerLevel++
     println(playerLevel)
     readBountyBoard()
+
+    val omSymbol: Char = '\u0950'
+
+
+
 }
 
 private fun readBountyBoard() {
@@ -46,30 +51,30 @@ private fun obtainQuest(
     playerClass: String = "paladin",
     hasAngeredBarbarians: Boolean = false,
     hasBefriendedBarbarians: Boolean = true,
-
-    ): String = when (playerLevel) {
-        1 -> "Meet Mr. Bubbles in the land of soft things."
-        in 2..5 -> {
-            val canTalkToBarbarians = !hasAngeredBarbarians &&
-                    (hasBefriendedBarbarians || playerClass == "barbarian")
-            if (canTalkToBarbarians) {
-                "Convince the barbarians to call off their invasion."
-            } else {
-                "Save the town from the barbarian invasions."
-            }
+): String = when (playerLevel) {
+    1 -> "Meet Mr. Bubbles in the land of soft things."
+    in 2..5 -> {
+        val canTalkToBarbarians = !hasAngeredBarbarians &&
+                (hasBefriendedBarbarians || playerClass == "barbarian")
+        if (canTalkToBarbarians) {
+            "Convince the barbarians to call off their invasion."
+        } else {
+            "Save the town from the barbarian invasions."
         }
-
-        6 -> "Locate the enchanted sword."
-        7 -> "Recover the long-lost artifact of creation."
-        8 -> "Defeat Nogartse, bring of death and eater of worlds."
-        else -> "There are no quests right now."
     }
-    //return quest
-    fun forgeItem(
-        quantity: Int = 1,
-        itemName: String = "sword",
-        material: String,
-        jewels: Boolean = false,
+
+    6 -> "Locate the enchanted sword."
+    7 -> "Recover the long-lost artifact of creation."
+    8 -> "Defeat Nogartse, bring of death and eater of worlds."
+    else -> "There are no quests right now."
+}
+//return quest
+fun forgeItem(
+    quantity: Int = 1,
+    itemName: String = "sword",
+    material: String,
+    jewels: Boolean = false,
     ): String{
         TODO("Implement the strings returned for forgeItem")
-    }
+}
+
