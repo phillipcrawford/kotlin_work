d7aad0e4fb12326f67c95048bcdc9cd10b3e86c7
Phillip Crawford <phillip.crawford3@gmail.com>
2024-12-27 21:57:24 -0700

    qwertyfg

--- bounty-board/src/main/kotlin/main.kt
+++ bounty-board/src/main/kotlin/main.kt
@@ -39,7 +39,7 @@ fun main() {
     print(omSymbol)
 
     var quest: String = "Rescue the princess"
-    quest = nullqwertyuiopasdf
+    quest = nullqwertyuiopasdfg
 }
 
 private fun readBountyBoard() {
